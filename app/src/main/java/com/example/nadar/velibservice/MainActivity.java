4e62b2a4682776f307393975e08d307227d2c5b7
Think <ulziiburend@gmail.com>
2018-12-18 16:04:41 +0100

    last

--- app/src/main/java/com/example/nadar/velibservice/MainActivity.java
+++ app/src/main/java/com/example/nadar/velibservice/MainActivity.java
@@ -1,31 +1,51 @@
 package com.example.nadar.velibservice;
 
 import android.app.ActivityManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.nadar.velibservice.model.InfoStation;
+import com.example.nadar.velibservice.model.ListeDesStationsVelib;
+import com.example.nadar.velibservice.model.StationVelib;
+
+import java.io.InputStream;
+import java.net.URL;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     Intent intent;
     boolean started;
-    ServiceStation timeService;
+    ServiceStation timeService; 
+    ListView list;
 
+    private ListeDesStationsVelib stations;
     private ServiceConnection connection = new ServiceConnection() {
 
         public void onServiceConnected(ComponentName className,
@@ -47,19 +67,87 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Button getCount = (Button) findViewById(R.id.getCount);
-        final EditText countText = (EditText) findViewById(R.id.countText);
 
-        getCount.setOnClickListener(new View.OnClickListener() {
+
+
+        list= (ListView) findViewById(R.id.stationsList);
+
+        try {
+            this.stations = new ListeDesStationsVelib();
+        } catch (Exception e) {
+        }
+        new ChargeurListeDesStations().execute("http://www.velib.paris.fr/service/carto");
+
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View v) {
-                if(started)
-                    countText.setText("count: " + timeService.getCount());
-                else
-                    countText.setText("service non démarré");
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = ((TextView) view).getText().toString();
+                StationVelib st = stations.lireStation(item);
+                new ChargeurInfoStation(st).execute();
 
             }
         });
+
+    }
+
+
+    private class ChargeurInfoStation extends AsyncTask<Void, Void, Boolean> {
+
+        private StationVelib stationVelib;
+        private InfoStation infoStation;
+        public ChargeurInfoStation(StationVelib str) {
+            this.stationVelib = str;
+        }
+
+
+        @Override
+        protected Boolean doInBackground(Void... voids) {
+            infoStation= new InfoStation(stationVelib.getNumber());
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            super.onPostExecute(aBoolean);
+            SharedPreferences sharedPreferences =getSharedPreferences("ahmad",0);
+            sharedPreferences.edit().putString("slots","<"+infoStation.getAvailable()+","+infoStation.getFree()+">").apply();
+
+        }
+    }
+
+
+    private class ChargeurListeDesStations extends AsyncTask<String, Void, Boolean> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Boolean doInBackground(String... str) {
+            try {
+
+                URL url = new URL(str[0]);
+                stations.chargerDepuisXML(url.openStream());
+            } catch (Exception e) {
+                e.printStackTrace();
+                try {
+                    InputStream is = MainActivity.this.getAssets().open("stations.xml");
+                    stations.chargerDepuisXML(is);
+                } catch (Exception e1) {
+                    e1.printStackTrace();
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            super.onPostExecute(aBoolean);
+            list.setAdapter(new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,stations.lesNomsDesStations()));
+        }
     }
 
     @Override
@@ -110,4 +198,6 @@ public class MainActivity extends AppCompatActivity {
         }
         return res;
     }
+
+
 }
--- app/src/main/java/com/example/nadar/velibservice/ServiceStation.java
+++ app/src/main/java/com/example/nadar/velibservice/ServiceStation.java
@@ -1,67 +1,108 @@
 package com.example.nadar.velibservice;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Binder;
+import android.os.Build;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 
 import java.util.concurrent.atomic.AtomicLong;
 
 public class ServiceStation extends Service {
+    private NotificationManager mNM;
 
+    // Unique Identification Number for the Notification.
+    // We use it on Notification start, and to cancel it.
+    private int NOTIFICATION = R.string.local_service_started;
     private static Thread ticker;
-    private static AtomicLong count;
     final IBinder binder = new LocalBinder();
 
     @Override
     public IBinder onBind(Intent intent) {
-        return  binder;
+        return binder;
     }
 
     @Override
-    public void onCreate(){
-            count = new AtomicLong();
-            ticker = new Thread( new Ticker());
-            ticker.start();
+    public void onCreate() {
+        mNM = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        ticker = new Thread(new Ticker());
+
+        ticker.start();
     }
 
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId){
+    public int onStartCommand(Intent intent, int flags, int startId) {
         return START_STICKY;
     }
 
     @Override
-    public void onDestroy()
-    {
+    public void onDestroy() {
         super.onDestroy();
+        mNM.cancel(NOTIFICATION);
+
     }
 
-    public long getCount(){
-        return count.get();
-    }
 
-    private class Ticker implements Runnable{
+    private class Ticker implements Runnable {
 
         @Override
         public void run() {
-            Log.i("Ticker Interrupted?",ticker.isInterrupted()+"");
-            while(!ticker.isInterrupted()){
-                try{
+            Log.i("Ticker Interrupted?", ticker.isInterrupted() + "");
+            while (!ticker.isInterrupted()) {
+                try {
                     Thread.sleep(1000);
-                    count.set(count.longValue()+1L);
-                }catch(Exception e){
+//                    callNotification();
+                    showNotification();
+                } catch (Exception e) {
                     return;
                 }
             }
         }
     }
 
+
     public class LocalBinder extends Binder {
         ServiceStation getService() {
-            Log.i("ServiceStation","getService");
+            Log.i("ServiceStation", "getService");
             return ServiceStation.this;
         }
     }
 
+    private void showNotification() {
+        SharedPreferences sharedPreferences = getSharedPreferences("ahmad", 0);
+
+        Log.e("callNotification", sharedPreferences.getString("slots", "please choose station"));
+
+        CharSequence text = sharedPreferences.getString("slots", "please choose station");
+
+
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, MainActivity.class), 0);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            /* Create or update. */
+            NotificationChannel channel = new NotificationChannel("my_channel_01",
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_HIGH);
+            mNM.createNotificationChannel(channel);
+        }
+        NotificationCompat.Builder notification = new NotificationCompat.Builder(this,"my_channel_01")
+                .setSmallIcon(R.mipmap.ic_launcher)  // the status icon
+                .setContentTitle("Velib service")  // the label of the entry
+                .setContentText(text)  // the contents of the entry
+                .setContentIntent(contentIntent)  ;// The intent to send when the entry is clicked
+
+
+        // Send the notification.
+        mNM.notify(R.string.local_service_started, notification.build());
+    }
 }
